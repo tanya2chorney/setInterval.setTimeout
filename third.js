dde8af5445fe41594cb42d0c6f3513d76754d371
tanya2chorney <tanyachorney5@gmail.com>
2024-12-17 12:12:45 +0200

    fix

--- third.js
+++ third.js
@@ -1,4 +1,3 @@
-let totalScore = 0;
 let clicks = 0;
 const clickBtn = document.getElementById("click-btn");
 const restartBtn = document.getElementById("restart-btn");
@@ -9,17 +8,18 @@ const gameDuration = 10000;
 clickBtn.addEventListener("click", () => {
   clicks++;
   clicksDisplay.textContent = `Скільки разів ви натиснули за цю гру: ${clicks}`;
+  scoreDisplay.textContent = `Ваші бали: ${clicks}`;
 });
 restartBtn.addEventListener("click", startGame);
 function startGame() {
   clicks = 0;
+  scoreDisplay.textContent = `Ваші бали: ${clicks}`;
   clicksDisplay.textContent = `Скільки разів ви натиснули за цю гру: 0`;
+
   restartBtn.style.display = "none";
   clickBtn.style.display = "block";
   const intervalId = setInterval(() => {
     clearInterval(intervalId);
-    totalScore += clicks;
-    scoreDisplay.textContent = `Ваші бали: ${totalScore}`;
     clickBtn.style.display = "none";
     restartBtn.style.display = "block";
   }, gameDuration);
